5529cd44f0abd9169fad85032419b14d5a4423d2
Peter Pan <apppeterpan@gmail.com>
2016-08-15 17:43:02 +0800

    Initial Commit

---
+++ NotificationDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotificationDemo
+//
+//  Created by Peter Pan on 8/15/16.
+//  Copyright © 2016 Peter Pan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
